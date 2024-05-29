0dea0f0689f6ed5e0ec707f1021d8aefaedef074
TobiasTheDanish <t.hc199811@gmail.com>
2024-05-29 23:09:07 +0200

    refactor ExternalIP method

--- tcp_server/internal/ip/ip.go
+++ tcp_server/internal/ip/ip.go
@@ -1,43 +1,18 @@
 package ip
 
 import (
-	"errors"
 	"net"
 )
 
+// Get preferred outbound ip of this machine
 func ExternalIP() (string, error) {
-	ifaces, err := net.Interfaces()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		return "", err
 	}
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			continue // interface down
-		}
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue // loopback interface
-		}
-		addrs, err := iface.Addrs()
-		if err != nil {
-			return "", err
-		}
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue // not an ipv4 address
-			}
-			return ip.String(), nil
-		}
-	}
-	return "", errors.New("are you connected to the network?")
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String(), nil
 }
