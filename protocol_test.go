31dc4035fc7292865e16706a25d6d2a71b9acd94
TobiasTheDanish <t.hc199811@gmail.com>
2024-06-11 23:56:19 +0200

    impl all currently supported types for IntoType func

--- protocol_test.go
+++ protocol_test.go
@@ -154,14 +154,14 @@ func TestParsePacket(t *testing.T) {
 }
 
 type testStruct struct {
-	name string
-	age  uint32
+	Name string
+	Age  uint32
 }
 
 func TestPacketFromStruct(t *testing.T) {
 	test := testStruct{
-		name: "Tobias",
-		age:  3_000_000,
+		Name: "Tobias",
+		Age:  3_000_000,
 	}
 	packet, err := shared.PacketFromType(test)
 	if err != nil {
@@ -174,21 +174,28 @@ func TestPacketFromStruct(t *testing.T) {
 		return
 	}
 
-	expectedDataLength := uint16(len(test.name) + int(unsafe.Sizeof(test.age)))
+	expectedDataLength := uint16(len(test.Name) + int(unsafe.Sizeof(test.Age)) + 1)
 	if packet.Header.DataLength != expectedDataLength {
 		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
+		return
 	}
 
 	index := 0
-	for i := range len(test.name) {
-		if test.name[i] != packet.Data[index] {
-			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, test.name[i], packet.Data[index]))
+	strLen := packet.Data[index]
+	index += 1
+	if int(strLen) != len(test.Name) {
+		t.Errorf("Incorrect string length, expected %d, got %d\n", len(test.Name), strLen)
+		return
+	}
+	for i := range len(test.Name) {
+		if test.Name[i] != packet.Data[index] {
+			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, test.Name[i], packet.Data[index]))
 		}
 
 		index += 1
 	}
-	for i := range unsafe.Sizeof(test.age) {
-		val := byte(test.age >> (8 * (3 - i)))
+	for i := range unsafe.Sizeof(test.Age) {
+		val := byte(test.Age >> (8 * (3 - i)))
 		if val != packet.Data[index] {
 			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, val, packet.Data[index]))
 		}
@@ -198,14 +205,14 @@ func TestPacketFromStruct(t *testing.T) {
 }
 
 type nestedTestStruct struct {
-	data struct {
-		fame int16
+	Data struct {
+		Fame int16
 	}
 }
 
 func TestPacketFromNestedStruct(t *testing.T) {
 	test := nestedTestStruct{
-		data: struct{ fame int16 }{fame: -345},
+		Data: struct{ Fame int16 }{Fame: -345},
 	}
 	packet, err := shared.PacketFromType(test)
 	if err != nil {
@@ -219,17 +226,17 @@ func TestPacketFromNestedStruct(t *testing.T) {
 	}
 	index := 0
 
-	expectedDataLength := uint16(unsafe.Sizeof(test.data.fame))
+	expectedDataLength := uint16(unsafe.Sizeof(test.Data.Fame))
 	if packet.Header.DataLength != expectedDataLength {
 		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
 	}
 
 	dataFame := int16(packet.Data[index])<<8 | int16(packet.Data[index+1])
-	if test.data.fame != dataFame {
-		t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, test.data.fame, dataFame))
+	if test.Data.Fame != dataFame {
+		t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, test.Data.Fame, dataFame))
 	}
 	for i := range 2 {
-		val := byte(test.data.fame >> (8 * (1 - i)))
+		val := byte(test.Data.Fame >> (8 * (1 - i)))
 		if val != packet.Data[index] {
 			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, val, packet.Data[index]))
 		}
@@ -253,9 +260,17 @@ func TestPacketFromByteSlice(t *testing.T) {
 	}
 	index := 0
 
-	expectedDataLength := uint16(len(data))
+	expectedDataLength := uint16(len(data) + 1)
 	if packet.Header.DataLength != expectedDataLength {
 		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
+		return
+	}
+
+	strLen := packet.Data[index]
+	index += 1
+	if int(strLen) != len(data) {
+		t.Errorf("Incorrect slice length, expected %d, got %d\n", len(data), strLen)
+		return
 	}
 
 	for i := range len(data) {
@@ -281,11 +296,18 @@ func TestPacketFromArray(t *testing.T) {
 	}
 	index := 0
 
-	expectedDataLength := uint16(len(truthTable))
+	expectedDataLength := uint16(len(truthTable) + 1)
 	if packet.Header.DataLength != expectedDataLength {
 		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
+		return
 	}
 
+	strLen := packet.Data[index]
+	index += 1
+	if int(strLen) != len(truthTable) {
+		t.Errorf("Incorrect slice length, expected %d, got %d\n", len(truthTable), strLen)
+		return
+	}
 	for i := range len(truthTable) {
 		expected := byte(0)
 		if truthTable[i] {
@@ -331,8 +353,8 @@ func TestPacketFromUintPtr(t *testing.T) {
 
 func TestPacketFromPointer(t *testing.T) {
 	data := testStruct{
-		name: "Tobias",
-		age:  256,
+		Name: "Tobias",
+		Age:  256,
 	}
 
 	packet, err := shared.PacketFromType(&data)
@@ -346,21 +368,27 @@ func TestPacketFromPointer(t *testing.T) {
 		return
 	}
 
-	expectedDataLength := uint16(len(data.name) + int(unsafe.Sizeof(data.age)))
+	expectedDataLength := uint16(len(data.Name) + int(unsafe.Sizeof(data.Age)) + 1)
 	if packet.Header.DataLength != expectedDataLength {
 		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
 	}
 
 	index := 0
-	for i := range len(data.name) {
-		if data.name[i] != packet.Data[index] {
-			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, data.name[i], packet.Data[index]))
+	strLen := packet.Data[index]
+	index += 1
+	if int(strLen) != len(data.Name) {
+		t.Errorf("Incorrect string length, expected %d, got %d\n", len(data.Name), strLen)
+		return
+	}
+	for i := range len(data.Name) {
+		if data.Name[i] != packet.Data[index] {
+			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, data.Name[i], packet.Data[index]))
 		}
 
 		index += 1
 	}
-	for i := range unsafe.Sizeof(data.age) {
-		val := byte(data.age >> (8 * (3 - i)))
+	for i := range unsafe.Sizeof(data.Age) {
+		val := byte(data.Age >> (8 * (3 - i)))
 		if val != packet.Data[index] {
 			t.Error(fmt.Sprintf("Error creating packet. At byte pos %d, expected %d, got %d", index, val, packet.Data[index]))
 		}
@@ -375,19 +403,14 @@ func TestPacketFromNilPointer(t *testing.T) {
 	data = nil
 
 	packet, err := shared.PacketFromType(data)
-	if err != nil {
-		t.Errorf("Did not expect error, but got: %s", err)
+	if err == nil {
+		t.Errorf("Expected unsupported type error, but got none.")
 		return
 	}
 
-	if packet == nil {
-		t.Error("Expected packet but got nil")
+	if packet != nil {
+		t.Error("Expected nil, but got packet.")
 		return
-	}
-
-	expectedDataLength := uint16(0)
-	if packet.Header.DataLength != expectedDataLength {
-		t.Errorf("Incorrect datalength, expected %d, got %d\n", expectedDataLength, packet.Header.DataLength)
 	}
 }
 
@@ -495,5 +518,231 @@ func TestPacketFromComplex128(t *testing.T) {
 	if packet != nil {
 		t.Error("Expected nil, but got packet.")
 		return
+	}
+}
+
+func TestPacketIntoStruct(t *testing.T) {
+	data := testStruct{
+		Name: "Jesus",
+		Age:  2024,
+	}
+
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	decoded := testStruct{
+		Name: "",
+		Age:  0,
+	}
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if decoded.Age != data.Age || decoded.Name != data.Name {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+	}
+}
+
+func TestPacketIntoNestedStruct(t *testing.T) {
+	data := nestedTestStruct{
+		Data: struct{ Fame int16 }{Fame: -345},
+	}
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	decoded := nestedTestStruct{Data: struct{ Fame int16 }{}}
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if decoded.Data.Fame != data.Data.Fame {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+	}
+}
+
+func TestPacketIntoSlice(t *testing.T) {
+	data := []byte{1, 255, 42, 69}
+
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	decoded := make([]byte, 0, 4)
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if len(decoded) != len(data) {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+		return
+	}
+
+	for i := range len(data) {
+		if decoded[i] != data[i] {
+			t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+			return
+		}
+	}
+}
+
+func TestPacketIntoArray(t *testing.T) {
+	data := [4]bool{true, false, false, true}
+
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	var decoded [4]bool
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if len(decoded) != len(data) {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+		return
+	}
+
+	for i := range len(data) {
+		if decoded[i] != data[i] {
+			t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+			return
+		}
+	}
+}
+
+func TestPacketIntoFloat32(t *testing.T) {
+	data := float32(67000.123)
+
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	var decoded float32
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if decoded != data {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+	}
+}
+
+func TestPacketIntoFloat64(t *testing.T) {
+	data := float64(67000.123)
+
+	packet, err := shared.PacketFromType(data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	var decoded float64
+
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if decoded != data {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+	}
+}
+
+func TestPacketIntoPointer(t *testing.T) {
+	data := testStruct{
+		Name: "Tobias",
+		Age:  256,
+	}
+
+	packet, err := shared.PacketFromType(&data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	decoded := &testStruct{}
+	err = packet.IntoType(&decoded)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+	}
+
+	if decoded.Age != data.Age || decoded.Name != data.Name {
+		t.Errorf("Decoded data malformed.\nExpected: %v\nGot: %v", data, decoded)
+	}
+}
+
+func TestPacketIntoNilPointer(t *testing.T) {
+	data := testStruct{
+		Name: "Tobias",
+		Age:  256,
+	}
+
+	packet, err := shared.PacketFromType(&data)
+	if err != nil {
+		t.Errorf("Did not expect error, but got: %s", err)
+		return
+	}
+
+	if packet == nil {
+		t.Error("Expected packet but got nil")
+		return
+	}
+
+	var decoded *testStruct
+	err = packet.IntoType(&decoded)
+	if err == nil {
+		t.Errorf("Expected error but got data: %v", decoded)
 	}
 }
--- shared/protocol.go
+++ shared/protocol.go
@@ -50,6 +50,194 @@ func (p *Packet) VersionString() string {
 	return fmt.Sprintf("%d.%d", major, minor)
 }
 
+func (p *Packet) IntoType(t interface{}) error {
+	if t == nil {
+		return errors.New("Cannot write packet into nil pointer")
+	}
+	rv := reflect.ValueOf(t)
+
+	if rv.Kind() != reflect.Pointer {
+		return errors.New("Cannot write packet into variable that is not a pointer")
+	}
+
+	elem := rv.Elem()
+	var byteIndex uint64
+	byteIndex = 0
+	err := p.setValue(&elem, &byteIndex)
+
+	return err
+}
+
+func (p *Packet) setValue(v *reflect.Value, byteIndex *uint64) error {
+	switch v.Kind() {
+	case reflect.Struct:
+		return p.setStruct(v, byteIndex)
+	case reflect.String:
+		return p.setString(v, byteIndex)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return p.setUint(v, byteIndex)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return p.setInt(v, byteIndex)
+	case reflect.Slice, reflect.Array:
+		return p.setSliceOrArray(v, byteIndex)
+	case reflect.Pointer, reflect.Interface:
+		return p.setPointerOrInterface(v, byteIndex)
+	case reflect.Bool:
+		return p.setBool(v, byteIndex)
+	case reflect.Float32, reflect.Float64:
+		return p.setFloat(v, byteIndex)
+	case reflect.Invalid:
+		return errors.Join(InvalidType, errors.New("If you are passing a pointer make sure that it is not nil."))
+	default:
+		return errors.Join(UnsupportedType, errors.New(fmt.Sprintf("Type '%s' is not currently supported.", v.Kind().String())))
+	}
+}
+
+func (p *Packet) setPointerOrInterface(v *reflect.Value, byteIndex *uint64) error {
+	elem := v.Elem()
+
+	return p.setValue(&elem, byteIndex)
+}
+
+func (p *Packet) setStruct(v *reflect.Value, byteIndex *uint64) error {
+	numFields := v.NumField()
+	for i := range numFields {
+		field := v.Field(i)
+
+		if !field.CanSet() {
+			return errors.New(fmt.Sprintf("Cannot set value of field '%s'. Make sure the field is exported.", field.String()))
+		}
+
+		err := p.setValue(&field, byteIndex)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *Packet) setSliceOrArray(v *reflect.Value, byteIndex *uint64) error {
+	numElem := p.Data[*byteIndex]
+	*byteIndex += 1
+	if !v.CanSet() {
+		return errors.New(fmt.Sprintf("Cannot set %s", v.String()))
+	}
+
+	var newV reflect.Value
+
+	if v.Kind() == reflect.Slice {
+		newV = reflect.MakeSlice(v.Type(), int(numElem), int(numElem))
+	} else if v.Kind() == reflect.Array {
+		if v.Len() != int(numElem) {
+			return errors.New(fmt.Sprintf("Mismatched length of array. Length in data: %d, expected length: %d", numElem, v.Cap()))
+		}
+
+		newV = *v
+	}
+
+	for i := range numElem {
+		elem := newV.Index(int(i))
+		err := p.setValue(&elem, byteIndex)
+		if err != nil {
+			return err
+		}
+	}
+
+	v.Set(newV)
+
+	return nil
+}
+
+func (p *Packet) setString(v *reflect.Value, byteIndex *uint64) error {
+	bIndex := *byteIndex
+	bytesToRead := p.Data[bIndex]
+	bIndex += 1
+	data := p.Data[bIndex : bIndex+uint64(bytesToRead)]
+
+	v.SetString(string(data))
+	*byteIndex = bIndex + uint64(bytesToRead)
+	return nil
+}
+
+func (p *Packet) setUint(v *reflect.Value, byteIndex *uint64) error {
+	bIndex := *byteIndex
+	size := uint64(v.Type().Bits())
+	bytesToRead := size / 8
+
+	if bytesToRead+bIndex > uint64(p.Header.DataLength) {
+		return errors.New("Data incompatible with mapping type. Trying to read to many bytes")
+	}
+
+	var newVal uint64
+	for i := range bytesToRead {
+		shiftVal := (8 * ((bytesToRead - 1) - i))
+		val := p.Data[bIndex+i]
+		newVal = (uint64(val) << shiftVal) | newVal
+	}
+
+	v.SetUint(newVal)
+	*byteIndex = bIndex + uint64(bytesToRead)
+	return nil
+}
+
+func (p *Packet) setInt(v *reflect.Value, byteIndex *uint64) error {
+	bIndex := *byteIndex
+	size := uint64(v.Type().Bits())
+	bytesToRead := size / 8
+
+	var newVal int64
+	for i := range bytesToRead {
+		shiftVal := (8 * ((bytesToRead - 1) - i))
+		val := p.Data[bIndex+i]
+		newVal = (int64(val) << shiftVal) | newVal
+	}
+
+	v.SetInt(newVal)
+	*byteIndex = bIndex + uint64(bytesToRead)
+	return nil
+}
+
+func (p *Packet) setFloat(v *reflect.Value, byteIndex *uint64) error {
+	bIndex := *byteIndex
+	size := uint64(v.Type().Bits())
+	bytesToRead := size / 8
+
+	bits := uint64(0)
+	for i := range bytesToRead {
+		shiftVal := (8 * ((bytesToRead - 1) - i))
+		val := p.Data[bIndex+i]
+		bits = (uint64(val) << shiftVal) | bits
+	}
+
+	var newVal float64
+	if bytesToRead == 8 {
+		newVal = math.Float64frombits(bits)
+	} else if bytesToRead == 4 {
+		newVal = float64(math.Float32frombits(uint32(bits)))
+	} else {
+		return errors.New(fmt.Sprintf("Invalid float size of %d bytes", bytesToRead))
+	}
+
+	v.SetFloat(newVal)
+
+	*byteIndex = bIndex + uint64(bytesToRead)
+	return nil
+}
+
+func (p *Packet) setBool(v *reflect.Value, byteIndex *uint64) error {
+	val := p.Data[*byteIndex]
+	b := false
+	if val == 1 {
+		b = true
+	}
+
+	v.SetBool(b)
+	*byteIndex += 1
+
+	return nil
+}
+
 func PacketFromType(t interface{}) (*Packet, error) {
 	if t == nil {
 		return PacketFromData([]byte{})
@@ -74,26 +262,25 @@ func getBytesFromValue(v reflect.Value) ([]byte, error) {
 	switch kind {
 	case reflect.Struct:
 		data, err = getBytesFromStruct(v)
-	case reflect.Slice, reflect.Array:
-		data, err = getBytesFromSliceOrArray(v)
-	case reflect.Pointer, reflect.Interface:
-		data, err = getBytesFromValue(v.Elem())
 	case reflect.String:
-		data = []byte(v.String())
-	case reflect.Bool:
-		data = getBytesFromBool(v)
+		strLen := byte(v.Len())
+		strBytes := []byte(v.String())
+		data = []byte{strLen}
+		data = append(data, strBytes...)
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		data = getBytesFromInt(v)
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		data = getBytesFromUint(v)
+	case reflect.Slice, reflect.Array:
+		data, err = getBytesFromSliceOrArray(v)
+	case reflect.Pointer, reflect.Interface:
+		data, err = getBytesFromValue(v.Elem())
+	case reflect.Bool:
+		data = getBytesFromBool(v)
 	case reflect.Float32, reflect.Float64:
 		data = getBytesFromFloat(v)
-	case reflect.Complex64, reflect.Complex128:
-		return nil, errors.Join(UnsupportedType, errors.New(fmt.Sprintf("Type %s is not currently supported", v.Kind().String())))
-	case reflect.Invalid:
-		return []byte{}, nil
 	default:
-		return data, errors.Join(InvalidType, errors.New(fmt.Sprintf("Could not get bytes from field of type: %s", v.Kind().String())))
+		return nil, errors.Join(UnsupportedType, errors.New(fmt.Sprintf("Type %s is not currently supported", v.Kind().String())))
 	}
 
 	return data, err
@@ -101,6 +288,7 @@ func getBytesFromValue(v reflect.Value) ([]byte, error) {
 
 func getBytesFromSliceOrArray(v reflect.Value) ([]byte, error) {
 	data := make([]byte, 0)
+	data = append(data, byte(v.Len()))
 
 	for i := range v.Len() {
 		b, err := getBytesFromValue(v.Index(i))
